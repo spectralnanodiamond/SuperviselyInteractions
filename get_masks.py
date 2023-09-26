f7da3881d7b39a858fefd40d4b050f1978d418c9
spectralnanodiamond <66789790+spectralnanodiamond@users.noreply.github.com>
2023-09-26 15:34:11 +0100

    modifications on metadata output

--- get_masks.py
+++ get_masks.py
@@ -1,17 +1,13 @@
 import supervisely_lib as sly
-import json
-import requests
 from PIL import Image
-#from torchvision import datasets, transforms
-#from torch.utils.data import DataLoader
 import os
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 import numpy as np
 import cv2, zlib, base64, io
 from PIL import Image
-import tifffile
-import csv
+import csv, pandas as pd
+from tqdm import tqdm
 
 #Following the description here:
 #https://developer.supervisely.com/getting-started/basics-of-authentication
@@ -48,34 +44,53 @@ def mask_2_base64(mask):
 
 
 # Download images and annotations
-out_dir = "data/annotated/"
-os.makedirs(out_dir, exist_ok=True)
+out_dir_images = "data/annotated_images/"
+os.makedirs(out_dir_images, exist_ok=True)
+out_dir_objects = "data/annotated_objects/"
+os.makedirs(out_dir_objects, exist_ok=True)
 dict_id_to_orignal_filename = {}
+list_of_objects = []
 
 for dataset in api.dataset.get_list(project.id):
-    for image in api.image.get_list(dataset.id):
+    for image in tqdm(api.image.get_list(dataset.id)):
         image_id = image.id
         ann = api.annotation.download(image.id).annotation
         # img = api.image.download_np(image.id)
         
+        # if objects (masks) exist for this image, download the objects and masks
         if ann["objects"] != []:
             img = api.image.download_np(image.id)
             dict_id_to_orignal_filename[image_id] = image.name
             img_output_filename = 'imageId_' + str(image_id)
-            np.save(out_dir + img_output_filename + '.npy', img)
+            np.save(out_dir_images + img_output_filename + '.npy', img)
             #could also use tifffile.imwrite('output.tiff', img) here
 
-            origin_coords = 'x0_' + str(ann['objects'][0]['bitmap']['origin'][0])
-            origin_coords += '_x1_' + str(ann['objects'][0]['bitmap']['origin'][1])
-            object_id = str(ann['objects'][0]['id'])
-            mask = base64_2_mask(ann["objects"][0]["bitmap"]["data"])
-            object_name = img_output_filename
-            object_name += '_objectId_' + object_id
-            object_name += '_origin_' + origin_coords
-            np.save(out_dir + object_name + '.npy', mask)
+            for i in range(len(ann["objects"])):
+                dict_this_object = {}
+                dict_this_object['objectId'] = ann['objects'][i]['id']
+                dict_this_object['imageId'] = image_id
+                dict_this_object['image_original_filename'] = image.name
+                dict_this_object['origin_x0'] = ann['objects'][i]['bitmap']['origin'][0]
+                dict_this_object['origin_x1'] = ann['objects'][i]['bitmap']['origin'][1]
+                
+                origin_coords = 'x0_' + str(dict_this_object['origin_x0'])
+                origin_coords += '_x1_' + str(dict_this_object['origin_x1'])
+                
+                mask = base64_2_mask(ann["objects"][i]["bitmap"]["data"])
+                object_name = img_output_filename
+                object_name += '_objectId_' + str(dict_this_object['objectId'])
+                object_name += '_origin_' + origin_coords
+                np.save(out_dir_objects + object_name + '.npy', mask)
+
+                dict_this_object['object_size0'] = mask.shape[0]
+                dict_this_object['object_size1'] = mask.shape[1]
+                list_of_objects.append(dict_this_object)
 
 out_metadata = "data/"
-with open(out_metadata + 'annotated_metadata.csv', 'w') as csvfile:
+with open(out_metadata + 'annotated_images_metadata.csv', 'w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['image_id', 'original_filename'])
     writer.writerows(dict_id_to_orignal_filename.items())
+
+df = pd.DataFrame(list_of_objects)
+df.to_csv(out_metadata + 'annotated_objects_metadata.csv', index=False)
